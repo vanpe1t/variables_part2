354bc6884ab4b0871c9a7ba43af8d3c3b26ed864
vanpe1t <makarov.maks.v@yandex.ru>
2022-10-21 19:57:34 +0300

    1st Task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int varInt = 1;
+        byte varByte = 1;
+        short varShort = 1;
+        long varLong = 1;
+        float varFloat = 1f;
+        double varDouble = 1.0;
+
+        System.out.println("Значение переменной varInt c типом int равно " + varInt);
+        System.out.println("Значение переменной byte c типом int равно " + varByte);
+        System.out.println("Значение переменной short c типом int равно " + varShort);
+        System.out.println("Значение переменной long c типом int равно " + varLong);
+        System.out.println("Значение переменной float c типом int равно " + varFloat);
+        System.out.println("Значение переменной double c типом int равно " + varDouble);
     }
 }
